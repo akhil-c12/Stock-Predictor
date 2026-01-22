ba5ab194354bf062a515c9ac0f4f9efaf6fc88f6
Chamala Akhileshwar Reddy <chamalaakhilreddy@gmail.com>
2026-01-22 20:24:42 +0530

    error correction

--- src/target.py
+++ src/target.py
@@ -2,11 +2,11 @@ import pandas as pd
 """this module creates the decisiontarget for model
    by comparing it with tomorrow's closing price"""
 def build_direction(df:pd.DataFrame)->pd.DataFrame:
-    df=df.copy()
+   df=df.copy()
     
-    df["close_tomorrow"]=df["Close"].shift(-1)
-    df["direction"]=(df["close_tomorrow"]>df["Close"]).astype(int)
-    df=df.dropna(subset=["direction"])#closing price is dropeed to prevent data leakage
-    df=df.iloc[:-1]
-    df.drop(columns=["close_tomorrow"],inplace=True)
-    return df
+   df["close_tomorrow"]=df["Close"].shift(-1)
+   df["direction"]=(df["close_tomorrow"]>df["Close"]).astype(int)
+   df=df.dropna(subset=["direction"])#closing price is dropeed to prevent data leakage
+   df=df.iloc[:-1]
+   df.drop(columns=["close_tomorrow"],inplace=True)
+   return df
